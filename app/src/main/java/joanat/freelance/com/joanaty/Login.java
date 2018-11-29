34a52dcf08c48cf5b0835a7cee9b4098eec8fea5
fci <momen.shahen2020@gmail.com>
2018-11-29 16:55:58 +0200

    Initial commit

---
+++ app/src/main/java/joanat/freelance/com/joanaty/Login.java
@@ -0,0 +1,30 @@
+package joanat.freelance.com.joanaty;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+public class Login extends AppCompatActivity {
+
+    EditText username, pass;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        pass = findViewById(R.id.pass);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, SwitchNav.class));
+            }
+        });
+    }
+}
